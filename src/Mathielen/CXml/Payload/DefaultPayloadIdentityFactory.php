37c2dc22baa12e78ed3930d343e18e3ab811d214
Markus Thielen <info@logicx.de>
2022-04-22 10:14:56 +0200

    add milliseconds to payload identity

--- src/Mathielen/CXml/Payload/DefaultPayloadIdentityFactory.php
+++ src/Mathielen/CXml/Payload/DefaultPayloadIdentityFactory.php
@@ -6,13 +6,26 @@ use Mathielen\CXml\Model\PayloadIdentity;
 
 class DefaultPayloadIdentityFactory implements PayloadIdentityFactoryInterface
 {
+
+	/**
+	 * @var callable|\Closure
+	 */
+	private $timeCallable;
+
+	public function __construct(callable $timeCallable = null)
+	{
+		$this->timeCallable = $timeCallable ?? function () {
+				return new \DateTime();
+			};
+	}
+
 	private static function generateNewPayloadId(\DateTime $timestamp): string
 	{
 		//The recommended implementation is:
 		//datetime.process id.random number@hostname
 		return \sprintf(
 			'%s.%s.%s@%s',
-			$timestamp->getTimestamp(),
+			$timestamp->format('U.v'), //include milliseconds
 			\getmypid(),
 			\mt_rand(1000, 9999),
 			\gethostname()
@@ -21,7 +34,7 @@ class DefaultPayloadIdentityFactory implements PayloadIdentityFactoryInterface
 
 	public function newPayloadIdentity(): PayloadIdentity
 	{
-		$timestamp = new \DateTime();
+		$timestamp = \call_user_func($this->timeCallable);
 		$payloadId = self::generateNewPayloadId($timestamp);
 
 		return new PayloadIdentity(

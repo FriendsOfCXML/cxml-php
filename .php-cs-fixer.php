850db13c7b047f84cbb081097063fa85e86b680d
Markus Thielen <info@logicx.de>
2023-02-20 20:05:52 +0100

    php-cs-fixer 'phpdoc_separation' => false

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -24,7 +24,7 @@ return $config
 		'random_api_migration' => true,
 		'pow_to_exponentiation' => true,
 		'combine_nested_dirname' => true,
-		'skip_unlisted_annotations' => true,
+		'phpdoc_separation' => false,
 		'@PHP74Migration' => true,
 		'global_namespace_import' => [
 			'import_classes' => false,

f0bc0275e21e60f9b7f3aed2d14702c35cb777d8
faaru-io <52386126+faaru-io@users.noreply.github.com>
2023-11-09 22:05:29 +0100

    bugfix: change regex to lazy quantifier in Serializer #patch (#18)
    * bugfix: change regex to lazy quantifier in Serializer #patch
    
    * code review fix #patch
    
    ---------
    
    Co-authored-by: Giovanni Piemontese <22853912+ynnoig@users.noreply.github.com>

--- tests/CXmlTest/Model/SerializerTest.php
+++ tests/CXmlTest/Model/SerializerTest.php
@@ -377,6 +377,18 @@ class SerializerTest extends TestCase
 			</Request>
 			</cXML>';
 
-        $this->assertXmlStringEqualsXmlString($expectedXml, $actualXml);
+	    $this->assertXmlStringEqualsXmlString($expectedXml, $actualXml);
+	}
+
+    public function testDeserializeOneRowXml(): void
+    {
+        $xml = '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE cXML SYSTEM "http://xml.cxml.org/schemas/cXML/1.1.008/cXML.dtd"><cXML timestamp="2022-06-07T10:09:56+00:00" payloadID="x.y.z"><Response><Status code="200" text="OK">Ping Response CXml</Status></Response></cXML>';
+
+        $serializer = Serializer::create();
+        $cXml = $serializer->deserialize($xml);
+
+        $resultingXml = $serializer->serialize($cXml);
+
+        $this->assertXmlStringEqualsXmlString($xml, $resultingXml);
     }
 }

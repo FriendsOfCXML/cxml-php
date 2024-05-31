f614996c5329b35281e526e33f0968d1d5c8ee58
Giovanni Piemontese <22853912+ynnoig@users.noreply.github.com>
2024-05-31 10:35:16 +0200

    BREAKING CHANGE: only support php 8.2+

---
+++ rector.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Symfony\Set\JMSSetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/src',
+        __DIR__.'/tests',
+    ])
+    // uncomment to reach your current PHP version
+    ->withSets([
+        JMSSetList::ANNOTATIONS_TO_ATTRIBUTES,
+    ])
+;

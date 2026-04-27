882d0c9968c4bf6f12d976157221867b4edd3ca5
Giovanni Piemontese <22853912+ynnoig@users.noreply.github.com>
2026-04-27 15:50:10 +0200

    style: fix rector cs #none

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 call_user_func(static function (): void {
     if (!is_file($autoloadFile = __DIR__ . '/../vendor/autoload.php')) {
         throw new RuntimeException('Did not find vendor/autoload.php. Did you run "composer install --dev"?');
